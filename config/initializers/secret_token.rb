9e64ecf4247b09b2c365615ee7386356e1450e5e
Geremia Taglialatela <tagliala.dev@gmail.com>
2014-02-15 15:07:20 +0100

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAdmin406::Application.config.secret_token = '6cebd94fffe338dea6cc26c44d2fea51e03239020a6eb88e02b06859abadf3548c90ece9c982651db4ae7c9f689ef0c6b255e694ee417d5f8a2c810a2284f73c'
